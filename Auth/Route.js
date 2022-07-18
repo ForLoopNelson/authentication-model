1d4298fcb1b3432a79950f8729d8a938c3f0c2fb
Cory Nelson <fullnelsontech@gmail.com>
2022-07-18 17:46:09 -0400

    second commit

--- Auth/Route.js
+++ Auth/Route.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const { register, login, update, deleteUser } = require("./Auth")
+const { register, login, update, deleteUser, getUsers } = require("./Auth")
 router.route("/register").post(register)
 router.route("/login").post(login)
 router.route("/update").put(update)
 router.route("/deleteUser").delete(deleteUser)
-// router.route("/getUsers").get(getUsers)
+router.route("/getUsers").get(getUsers)
 
 module.exports = router
