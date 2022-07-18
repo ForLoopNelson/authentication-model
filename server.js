37be49a5b2f6d362e4aba56d0c2b6c4a8a92f75a
Cory Nelson <fullnelsontech@gmail.com>
2022-07-18 17:45:20 -0400

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
+const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const connectDB = require("./db") //uses another file
+const { adminAuth, userAuth } = require("./middleware/auth")
 
 require("dotenv").config()
 
@@ -10,6 +12,19 @@ connectDB()
 
 app.use(express.json())
 app.use("/api/Auth", require("./auth/Route"))
+
+// Routes
+app.use("/api/auth", require("./Auth/route"))
+
+app.get("/", (req, res) => res.render("home"))
+app.get("/register", (req, res) => res.render("register"))
+app.get("/login", (req, res) => res.render("login"))
+app.get("/logout", (req, res) => {
+  res.cookie("jwt", "", { maxAge: "1" })
+  res.redirect("/")
+})
+app.get("/admin", adminAuth, (req, res) => res.render("admin"))
+app.get("/basic", userAuth, (req, res) => res.render("user"))
 
 const server = app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running`)
