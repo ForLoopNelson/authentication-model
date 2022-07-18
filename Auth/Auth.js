6768ac7e0b72fd757d19cfd5b72d5ce1da258995
Cory Nelson <fullnelsontech@gmail.com>
2022-07-18 17:44:58 -0400

    Second commit

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -1,5 +1,7 @@
 const User = require("../model/User")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const jwtSecret = process.env.TOKEN
 
 exports.register = async (req, res, next) => {
   const { username, password } = req.body
@@ -8,81 +10,111 @@ exports.register = async (req, res, next) => {
       .status(400)
       .json({ message: "Password less than six characters" })
   }
-  try {
-    //May need to remove try catch and update this section!!
-    bcrypt.hash(password, 10).then(
-      async (hash) =>
-        await User.create({
-          username,
-          password: hash,
-        }).then((user) =>
-          res.status(200).json({
-            message: "User successfully created",
-            user,
-          })
-        )
-    )
-  } catch (err) {
-    res.status(401).json({
-      message: "User creation not successfull",
-      error: error.message,
+  bcrypt.hash(password, 10).then(async (hash) => {
+    await User.create({
+      username,
+      password: hash,
     })
-  }
+      .then((user) => {
+        const maxAge = 3 * 60 * 60
+        const token = jwt.sign(
+          { id: user._id, username, role: user.role },
+          jwtSecret,
+          {
+            expiresIn: maxAge, // 3hrs
+          }
+        )
+        res.cookie("jwt", token, {
+          httpOnly: true,
+          maxAge: maxAge * 1000,
+        })
+        res.status(201).json({
+          message: "User successfully created",
+          user: user._id,
+          role: user.role,
+        })
+      })
+      .catch((error) =>
+        res.status(400).json({
+          message: "User not successfully created",
+          error: error.message,
+        })
+      )
+  })
 }
 
 exports.login = async (req, res, next) => {
   const { username, password } = req.body
+
+  // Check if username and password is provided
   if (!username || !password) {
     return res.status(400).json({
-      message: "Username or Password is emptty",
+      message: "Username or Password not present",
     })
   }
+
   try {
     const user = await User.findOne({ username })
+
     if (!user) {
-      res.status(401).json({
-        message: "Login unsuccessful",
+      res.status(400).json({
+        message: "Login not successful",
         error: "User not found",
       })
     } else {
+      // comparing given password with hashed password
       bcrypt.compare(password, user.password).then(function (result) {
-        result
-          ? res.status(200).json({
-              message: "Login successfull",
-              user,
-            })
-          : res.status(400).json({ message: "Invalid Login" })
+        if (result) {
+          const maxAge = 3 * 60 * 60
+          const token = jwt.sign(
+            { id: user._id, username, role: user.role },
+            jwtSecret,
+            {
+              expiresIn: maxAge, // 3hrs in sec
+            }
+          )
+          res.cookie("jwt", token, {
+            httpOnly: true,
+            maxAge: maxAge * 1000, // 3hrs in ms
+          })
+          res.status(201).json({
+            message: "User successfully Logged in",
+            user: user._id,
+            role: user.role,
+          })
+        } else {
+          res.status(400).json({ message: "Login not succesful" })
+        }
       })
     }
-  } catch {
+  } catch (error) {
     res.status(400).json({
-      messsage: "Something went wrong.",
+      message: "An error occurred",
       error: error.message,
     })
   }
 }
-
 exports.update = async (req, res, next) => {
   const { role, id } = req.body
+  // Verifying if role and id is presnt
   if (role && id) {
+    // Verifying if the value of role is admin
     if (role === "admin") {
+      // Finds the user with the id
       await User.findById(id)
         .then((user) => {
+          // Verifies the user is not an admin
           if (user.role !== "admin") {
             user.role = role
             user.save((err) => {
+              //Monogodb error checker
               if (err) {
-                res.status(400).json({
-                  message: "An error has occured",
-                  error: err,
-                  message,
-                })
+                return res
+                  .status("400")
+                  .json({ message: "An error occurred", error: err.message })
                 process.exit(1)
               }
-              res.status(201).json({
-                message: "Update was successfull",
-                user,
-              })
+              res.status("201").json({ message: "Update successful", user })
             })
           } else {
             res.status(400).json({ message: "User is already an Admin" })
@@ -91,7 +123,7 @@ exports.update = async (req, res, next) => {
         .catch((error) => {
           res
             .status(400)
-            .json({ message: "An error occured", error: error.message })
+            .json({ message: "An error occurred", error: error.message })
         })
     } else {
       res.status(400).json({
@@ -99,20 +131,36 @@ exports.update = async (req, res, next) => {
       })
     }
   } else {
-    res.status(400).json({
-      message: "Role or ID is missing",
-    })
+    res.status(400).json({ message: "Role or Id not present" })
   }
 }
-
 exports.deleteUser = async (req, res, next) => {
   const { id } = req.body
   await User.findById(id)
     .then((user) => user.remove())
-    .then((user) => res.status(201).json({ message: "User deleted", user }))
+    .then((user) =>
+      res.status(201).json({ message: "User successfully deleted", user })
+    )
     .catch((error) =>
       res
         .status(400)
-        .json({ message: "An error occured", error: error.message })
+        .json({ message: "An error occurred", error: error.message })
+    )
+}
+exports.getUsers = async (req, res, next) => {
+  await User.find({})
+    .then((users) => {
+      const userFunction = users.map((user) => {
+        const container = {}
+        container.username = user.username
+        container.role = user.role
+        container.id = user._id
+
+        return container
+      })
+      res.status(200).json({ user: userFunction })
+    })
+    .catch((err) =>
+      res.status(401).json({ message: "Not successful", error: err.message })
     )
 }
